f5b477b56bfdc2ea66dc75effa8ff6b71aa231b7
廖韋臣 <rax02270521@gmail.com>
2020-10-19 16:20:57 +0800

    Routes setting Jump page

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,10 @@ Route::get('/',function(){
     return'welcome';
 });
 
+Route::get('r1', function(){
+    return redirect('r2');
+});
 
-
+Route::get('r2', function(){
+    return view('welcome');
+});
